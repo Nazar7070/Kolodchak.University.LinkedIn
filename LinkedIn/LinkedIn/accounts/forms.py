b0a2837257d049e62a48dae51f46e8db220bdf4f
Nazar7070 <116301073+Nazar7070@users.noreply.github.com>
2024-03-12 22:39:47 +0200

    Delete LinkedIn directory

--- LinkedIn/LinkedIn/accounts/forms.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
-
-class SignUpForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'email', 'password1', 'password2')
