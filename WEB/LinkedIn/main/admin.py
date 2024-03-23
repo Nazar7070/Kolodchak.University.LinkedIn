060eb17b6f9bc0070594cdb329fbb740b0ef10b6
Nazar7070 <116301073+Nazar7070@users.noreply.github.com>
2024-03-23 20:53:43 +0200

    Delete WEB/LinkedIn directory

--- WEB/LinkedIn/main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Experience, Education, Skills, Connections, Messages, Posts
-
-admin.site.register(Experience)
-admin.site.register(Education)
-admin.site.register(Skills)
-admin.site.register(Connections)
-admin.site.register(Messages)
-admin.site.register(Posts)
